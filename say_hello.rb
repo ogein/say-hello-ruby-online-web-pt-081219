31243644af5f7e3c73472fbc53719d324a445011
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-18 13:58:14 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,7 @@
 # Build your say_hello method here
 
-def say_hello(name)
-  if name == nil 
-    puts "Hello Ruby Programmer"
-  else
-    puts "Hello #{name}!"
-  end
+def say_hello(name = "Ruby Programmer")
+  
+  puts "Hello #{name}!"
+  
 end
